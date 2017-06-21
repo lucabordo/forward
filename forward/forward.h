5ad0bab565909f194b412c0fdfc361bcc6809fde
lucas bordeaux <lucabordo@hotmail.co.uk>
2017-06-21 21:54:49 +0100

    Iterators are moved; collections and lambdas are referenced by const ref; iterator results are returned by value. We have a design problem in getting a result_type for lambdas.

--- forward/forward.h
+++ forward/forward.h
@@ -7,19 +7,39 @@
 
 namespace forward
 {
-/*	template <typename Iteratable>
-	class EnumerableFromIteratableRef;
+	#pragma region Enumerators
 
-	template <typename Enumerable, typename Filter>
-	class WhereEnumerable;
+	// Enumerators are conceptually as follows. 
+	// Enumerators are moved within more complex enumerators. They should be lightweight objects.
+	/*
+  	class AbstractEnumerator
+	{
+	public:
 
-	template <typename Enumerable, typename Map>
-	class SelectEnumerable;
+		static const bool is_enumerator = true;
+		using value_type = int;
+		
+		// True if there is a current value.
+		bool has_value() const;
+
+		// Compute the current value.
+		// Calling this twice might cause the same calculation to be redone or, worse, access to a moved object.
+		value_type get_value() const;
+
+		// Move to the next value, if any. 
+		void forward();
+	};
 	*/
 
-	
-	// No auto...
 
+	// An enumerator based on a forward-iteratable container, STL-styler
+	// Implements:
+	//
+	// for (auto it = begin(); it != end(); ++it)
+	// {
+	//     yield return *it;
+	// }
+	//
 	template <typename Iterator>
 	class EnumeratorFromIterator
 	{
@@ -39,10 +59,9 @@ namespace forward
 			return _current != _end;
 		}
 
-		// Convention: always returns a reference to something pre-computed.
-		// This is to avoid any re-computation
-		const value_type& get_value() const
+		value_type get_value() const
 		{
+			assert(has_value());
 			return *_current;
 		}
 
@@ -55,8 +74,146 @@ namespace forward
 	private:
 
 		Iterator _current;
-		const Iterator _end;
+		Iterator _end; // const, but the Iterator might be moved
 	};
+
+
+	// An enumerator that applies a function to all elements of an underlying enumerator.
+	// The function is assumed to be stateless, deterministic, and is only const-referred to.
+	// Implements:
+	//
+	//    for (; en.has_value(); en.forward())
+	//    {
+	//        yield return map(en.get_value());
+	//    }
+	//
+	template <typename Enumerator, typename Map>
+	class SelectEnumerator
+	{
+	public:
+
+		static const bool is_enumerator = true;
+		using value_type = typename Enumerator::value_type;
+
+		SelectEnumerator(Enumerator enumerator, const Map& map) :
+			_enumerator(std::move(enumerator)),
+			_map(map)
+		{
+		}
+
+		bool has_value() const
+		{
+			return _enumerator.has_value();
+		}
+
+		value_type get_value() const
+		{
+			assert(has_value());
+			return _map(_enumerator.get_value());
+		}
+
+		void forward()
+		{
+			assert(has_value());
+			_enumerator.forward();
+		}
+
+	private:
+
+		Enumerator _enumerator;
+		const Map& _map;
+	};
+
+
+	// An enumerator that only returns the objects from an underlying enumerator that pass a certain test, or filter.
+	// The function is assumed to be stateless, deterministic, and is only const-referred to.
+	// Implements:
+	//
+	//    for (; en.has_value(); en.forward())
+    //    {
+    //        auto current = en.get_value();
+	//        bool valid = filter(current);
+	//        if (valid)
+    //           yield return std::move(current);
+	//    }
+	//
+	template <typename Enumerator, typename Filter>
+	class WhereEnumerator
+	{
+	public:
+
+		static const bool is_enumerator = true;
+		using value_type = typename Enumerator::value_type;
+
+		WhereEnumerator(Enumerator enumerator, const Filter& filter) :
+			_enumerator(std::move(enumerator)),
+			_filter(filter),
+			_valid(true)
+		{
+			seek();
+		}
+
+		bool has_value() const
+		{
+			return _valid;
+		}
+
+		value_type get_value() const
+		{
+			assert(has_value());
+			return std::move(_current);
+		}
+
+		void forward()
+		{
+			assert(has_value());
+			_enumerator.forward();
+			seek();
+		}
+
+	private:
+
+		// Move until either end or position that passes the filter
+		void seek()
+		{
+			assert(_valid);
+
+			for (; _enumerator.has_value(); _enumerator.forward())
+			{
+				_current = _enumerator.get_value();
+				if (_filter(_current))
+					return;
+			}
+
+			_valid = false;
+		}
+
+
+	private:
+
+		value_type _current;
+		bool _valid;
+
+		Enumerator _enumerator;
+		const Filter& _filter;
+	};
+
+	#pragma endregion
+
+	#pragma region Enumerable
+
+	// Conceptually, enumerables are:
+	/*
+	class Enumerable 
+	{
+		static const bool is_enumerable = true;
+		using value_type = int;
+		using enumerator = AbstractEnumerator;
+
+		// Produce an enumerator over this content
+		enumerator get_enumerator() const;
+	};
+	*/
 
 
 	template <typename Iteratable>
@@ -65,7 +222,6 @@ namespace forward
 	public:
 
 		static const bool is_enumerable = true;
-
 		using iterator = typename Iteratable::const_iterator;
 		using value_type = typename iterator::value_type;
 		using enumerator = EnumeratorFromIterator<iterator>;
@@ -86,110 +242,6 @@ namespace forward
 	};
 
 
-	template <typename Enumerator, typename Map>
-	class SelectEnumerator
-	{
-	public:
-
-		static const bool is_enumerator = true;
-		using value_type = typename Enumerator::value_type;
-
-		SelectEnumerator(Enumerator&& enumerator, const Map& map) :
-			_enumerator(enumerator),
-			_map(map)
-		{
-			calculate_current_value();
-		}
-
-		bool has_value() const
-		{
-			return _enumerator.has_value();
-		}
-
-		const value_type& get_value() const
-		{
-			return _current;
-		}
-
-		void forward()
-		{
-			assert(has_value());
-			_enumerator.forward();
-			calculate_current_value();
-		}
-
-	private:
-
-		void calculate_current_value()
-		{
-			_valid = _enumerator.has_value();
-			if (_valid)
-				_current = _map(_enumerator.get_value());
-		}
-
-	private:
-
-		Enumerator _enumerator;
-		const Map& _map;
-
-		value_type _current; // default-constructible, therefore
-		bool _valid;
-	};
-
-
-	template <typename Enumerator, typename Filter>
-	class WhereEnumerator
-	{
-	public:
-
-		static const bool is_enumerator = true;
-		using value_type = typename Enumerator::value_type;
-
-		WhereEnumerator(Enumerator&& enumerator, const Filter& filter) :
-			_enumerator(enumerator),
-			_filter(filter)
-		{
-			seek();
-		}
-
-		bool has_value() const
-		{
-			return _enumerator.has_value();
-		}
-
-		const value_type& get_value() const
-		{
-			return _enumerator.get_value();
-		}
-
-		void forward()
-		{
-			assert(has_value());
-			_enumerator.forward();
-			seek();
-		}
-
-	private:
-
-		// Move until either end or position that passes the filter
-		void seek()
-		{
-			while (_enumerator.has_value())
-			{
-				if (_filter(_enumerator.get_value()))
-					break;
-				else
-					_enumerator.forward();
-			}
-		}
-
-
-	private:
-
-		Enumerator _enumerator;
-		const Filter& _filter;
-	};
-
 
 	template <typename Enumerable, typename Filter>
 	class WhereEnumerable
@@ -200,7 +252,7 @@ namespace forward
 		using value_type = typename Enumerable::value_type;
 		using enumerator = WhereEnumerator<typename Enumerable::enumerator, Filter>;
 
-		WhereEnumerable(const Enumerable& enumerable, Filter filter) :
+		WhereEnumerable(const Enumerable& enumerable, const Filter& filter) :
 			_enumerable(enumerable),
 			_filter(std::move(filter))
 		{
@@ -214,7 +266,7 @@ namespace forward
 	private:
 
 		const Enumerable& _enumerable;
-		Filter _filter;
+		const Filter& _filter;
 	};
 
 
@@ -224,10 +276,12 @@ namespace forward
 	public:
 
 		static const bool is_enumerable = true;
-		using value_type = typename Enumerable::value_type;
+		using value_type = typename Map::result_type;
+//		using value_type = decltype();
+//			typename Enumerable::value_type;
 		using enumerator = SelectEnumerator<typename Enumerable::enumerator, Map>;
 
-		SelectEnumerable(const Enumerable& enumerable, Map filter) :
+		SelectEnumerable(const Enumerable& enumerable, const Map& filter) :
 			_enumerable(enumerable),
 			_map(std::move(filter))
 		{
@@ -241,9 +295,10 @@ namespace forward
 	private:
 
 		const Enumerable& _enumerable;
-		Map _map;
+		const Map& _map;
 	};
 
+	#pragma endregion
 
 
 	template <typename Iteratable>
@@ -253,55 +308,26 @@ namespace forward
 	}
 
 
-	// Ugly function for test
-	template <typename Enumerable>
-	std::vector<typename Enumerable::value_type> enumerable_to_vector(const Enumerable& enumerable)
-	{
-		static_assert(Enumerable::is_enumerable, "Oops.");
-		std::vector<typename Enumerable::value_type> result;
-		for (auto enumerator = enumerable.get_enumerator(); enumerator.has_value(); enumerator.forward())
-			result.push_back(enumerator.get_value());
-		return result;
-	}
-
-
-	// Ugly function for test
-	template <typename Enumerable, typename Filter>
-	auto where_from_enumerable(const Enumerable& enumerable, Filter filter)
-	{
-		return WhereEnumerable<Enumerable, Filter>(enumerable, std::move(filter));
-	}
-
-
-	// Ugly function for test
-	template <typename Enumerable, typename Map>
-	auto select_from_enumerable(const Enumerable& enumerable, Map map)
-	{
-		return SelectEnumerable<Enumerable, Map>(enumerable, std::move(map));
-	}
-
-
 	// Allow right hand side composition for where
 	template <typename Filter>
 	class WhereRightHandSide
 	{
 	private:
-		Filter _filter;
+		const Filter& _filter;
 	public:
-		WhereRightHandSide(Filter filter) : _filter(std::move(filter)) {}
+		WhereRightHandSide(const Filter& filter) : _filter(filter) {}
 
 		template <typename Enumerable>
 		WhereEnumerable<Enumerable, Filter> apply(const Enumerable& enumerable) const
 		{
-			// Avoid copying this filter?
 			return WhereEnumerable<Enumerable, Filter>(enumerable, _filter);
 		}
 	};
 
 	template <typename Filter>
-	WhereRightHandSide<Filter> where(Filter filter) 
+	WhereRightHandSide<Filter> where(const Filter& filter) 
 	{
-		return WhereRightHandSide<Filter>(std::move(filter));
+		return WhereRightHandSide<Filter>(filter);
 	}
 
 	template <typename Enumerable, typename Filter>
@@ -316,22 +342,21 @@ namespace forward
 	class SelectRightHandSide
 	{
 	private:
-		Map _map;
+		const Map& _map;
 	public:
-		SelectRightHandSide(Map map) : _map(std::move(map)) {}
+		SelectRightHandSide(const Map& map) : _map(map) {}
 
 		template <typename Enumerable>
 		SelectEnumerable<Enumerable, Map> apply(const Enumerable& enumerable) const
 		{
-			// Avoid copying this filter?
 			return SelectEnumerable<Enumerable, Map>(enumerable, _map);
 		}
 	};
 
 	template <typename Map>
-	SelectRightHandSide<Map> select(Map map)
+	SelectRightHandSide<Map> select(const Map& map)
 	{
-		return SelectRightHandSide<Map>(std::move(map));
+		return SelectRightHandSide<Map>(map);
 	}
 
 	template <typename Enumerable, typename Map>
@@ -342,38 +367,6 @@ namespace forward
 
 
 	// Some end point functions that do not produce an Enumerable
-
-	//template <typename Accumulator>
-	//class FoldRightHandSide
-	//{
-	//private:
-	//	
-	//	Accumulator _accumulator;
-
-	//public:
-
-	//	FoldRightHandSide(Accumulator accumulator) :
-	//		_accumulator(std::move(accumulator))
-	//	{
-	//	}
-
-	//	template <typename Enumerable>
-	//	auto apply(const Enumerable& enumerable) const
-	//	{
-	//		static_assert(Enumerable::is_enumerable, "Oops.");
-	//		auto result = _accumulator; // copy for sanity?
-	//		for (auto enumerator = enumerable.get_enumerator(); enumerator.has_value(); enumerator.forward())
-	//			_accumulator.accumulate(enumerator.get_value());
-	//		return _accumulator.result();
-	//	}
-	//};
-
-	//template <typename Enumerable, typename Accumulator>
-	//auto operator >> (const Enumerable& enumerable, const FoldRightHandSide<Accumulator>& fold)
-	//{
-	//	return fold.apply(enumerable);
-	//}
-
 
 	template <typename T>
 	class ToVector
@@ -403,50 +396,5 @@ namespace forward
 	{
 		return ToVector<T>();
 	}
-
-
-
-	/*
-	template <typename Enumerable>
-	ToVector<Enumerable::value_type> to_vector(const Enumer)
-	{
-		return ToVector<Enumerable::value_type>();
-	}
-
-
-	template <typename Initializer, typename Action>
-	class FoldRightHandSide
-	{
-	private:
-
-		Initializer _initializer; // RENAME
-		Action _action;
-
-	public:
-
-		FoldRightHandSide(Initializer initializer, Action action) :
-			_initializer(std::move(initializer)),
-			_action(std::move(action))
-		{
-		}
-
-		template <typename Enumerable>
-		auto apply(const Enumerable& enumerable) const
-		{
-			static_assert(Enumerable::is_enumerable, "Oops.");
-			auto result = _initializer;
-			for (auto enumerator = enumerable.get_enumerator(); enumerator.has_value(); enumerator.forward())
-				_action(enumerator.get_value());
-			return result;
-		}
-	};
-
-	template <typename Enumerable, typename Initializer, typename Action>
-	auto operator >> (const Enumerable& enumerable, const FoldRightHandSide<Initializer, Action>& fold)
-	{
-		return fold.apply(enumerable);
-	}
-*/
-
 }
 
--- forward/unittest1.cpp
+++ forward/unittest1.cpp
@@ -2,6 +2,7 @@
 #include "CppUnitTest.h"
 
 #include <vector>
+#include <string>
 
 #include "forward.h"
 
@@ -13,61 +14,14 @@ namespace forward
 	{
 	public:
 		
-		TEST_METHOD(TestEnumeratorFromIterator)
-		{
-			using namespace forward;
-			std::vector<int> v{15, 21};
-
-			auto en = from(v);
-			std::vector<int> copy = enumerable_to_vector(en);
-
-			assert(copy.size() == 2);
-			assert(copy[0] == v[0]);
-			assert(copy[1] == v[1]);
-		}
-
-
-		TEST_METHOD(TestWhere)
-		{
-			using namespace forward;
-			std::vector<int> v{ 15, 21 };
-			auto selector = [](int i) { return i < 20; };
-
-			auto en = from(v);
-			auto selection = where_from_enumerable(en, selector);
-
-			auto result = enumerable_to_vector(selection);
-
-			assert(result.size() == 1);
-			assert(result[0] == 15);
-		}
-
-
-		TEST_METHOD(TestSelect)
-		{
-			using namespace forward;
-			std::vector<int> v{ 15, 21 };
-			auto selector = [](int i) { return i + 3; };
-
-			auto en = from(v);
-			auto selection = select_from_enumerable(en, selector);
-
-			auto result = enumerable_to_vector(selection);
-
-			assert(result.size() == 2);
-			assert(result[0] == 18);
-			assert(result[1] == 24);
-		}
-
 		TEST_METHOD(Composition1)
 		{
 			using namespace forward;
 			std::vector<int> v{ 15, 21 };
 
-			auto en = from(v)
-				>> where([](int i) { return i < 20; });
-
-			auto result = enumerable_to_vector(en);
+			auto result = from(v)
+				>> where([](int i) { return i < 20; })
+				>> to_vector<int>();
 
 			assert(result.size() == 1);
 			assert(result[0] == 15);
@@ -78,16 +32,36 @@ namespace forward
 			using namespace forward;
 			std::vector<int> v{ 15, 21 };
 
+			std::function<int(int)> f;
+			decltype(f)::result_type;
+
 			auto result = 
 				from(v)
 				>> where([](int i) { return i < 20; })
 				>> select([](int i) { return i + 100; })
 				>> to_vector<int>();
 
-//			auto result = enumerable_to_vector(en);
-
 			assert(result.size() == 1);
 			assert(result[0] == 115);
+		}
+
+		TEST_METHOD(StringSizes)
+		{
+			using namespace forward;
+			std::vector<std::string> v{ "cat", "bunny", "doggy" };
+
+			auto lambda = [](int x) {return x + 1; };
+
+			auto result =
+				from(v)
+				>> where([](const std::string& s) { return s[0] < 'h'; })
+				>> select([](const std::string& s) { return s.size(); })
+				>> to_vector<int>();
+
+			assert(result.size() == 3);
+//			assert(result[0] == 3);
+	//		assert(result[1] == 5);
+		//	assert(result[2] == 5);
 		}
 
 		// Interating counter-examples
